d10ae16e3d0ffdb6899054c4799c0bb25aa985f6
Alexey Solovyev <solovyev.alexey@gmail.com>
2024-11-14 21:21:20 -0700

    Update Lemma65

--- LNS/Lemma65.lean
+++ LNS/Lemma65.lean
@@ -1,20 +1,19 @@
-import LNS.Common
 import LNS.Tactic
-import LNS.Basic
-set_option maxHeartbeats 10000000
+import LNS.Definitions
+import LNS.BasicErrorCorrection
 
-open LNS
-
-open Real
+namespace LNS
 
 noncomputable section
 
+open Real
+
 private def Gm a b := Fm b a / deriv (Fm b) a
 
-private def Km a b := -a * a * log (1-a) + a * a * log (b-a) + a * b - a + b * log (1-a) + b * log b - b * log (b-a)
+private def Km a b := -a * a * log (1 - a) + a * a * log (b - a) + a * b - a + b * log (1-a) + b * log b - b * log (b - a)
 
-lemma deriv_Km (ha: a ∈ (Set.Ioo 0 1)): Set.EqOn (deriv (Km a))
-      (fun b=> (a*a)/(b-a) + a - b/(b-a) + log b + log (1-a) - log (b-a) + (1:ℝ)) (Set.Ici 1) := by
+private lemma deriv_Km (ha : a ∈ Set.Ioo 0 1) : Set.EqOn (deriv (Km a))
+    (fun b => (a * a) / (b - a) + a - b / (b - a) + log b + log (1 - a) - log (b - a) + 1) (Set.Ici 1) := by
   unfold Km
   get_deriv (fun b ↦ -a * a * log (1 - a) + a * a * log (b - a) + a * b - a + b * log (1 - a) + b * log b - b * log (b - a))
       within (Set.Ici 1)
@@ -27,8 +26,8 @@ lemma deriv_Km (ha: a ∈ (Set.Ioo 0 1)): Set.EqOn (deriv (Km a))
   simp only [Set.mem_Ioo, Set.mem_Ici] at ha hb
   field_simp; ring_nf
 
-lemma deriv2_Km (ha: a ∈ (Set.Ioo 0 1)):  Set.EqOn (deriv (deriv (Km a)))
-      (fun b=> -((a^2*(b-1))/(b*(b-a)^2))) (Set.Ioi 1) := by
+private lemma deriv2_Km (ha : a ∈ Set.Ioo 0 1) : Set.EqOn (deriv (deriv (Km a)))
+    (fun b => -((a ^ 2 * (b - 1)) / (b * (b - a) ^ 2))) (Set.Ioi 1) := by
   have e : Set.EqOn (deriv (Km a))
       (fun b=> (a*a)/(b-a) + a - b/(b-a) + log b + log (1-a) - log (b-a) + (1:ℝ)) (Set.Ioi 1) := by
     apply Set.EqOn.mono _ (deriv_Km ha)
@@ -44,7 +43,7 @@ lemma deriv2_Km (ha: a ∈ (Set.Ioo 0 1)):  Set.EqOn (deriv (deriv (Km a)))
   have : b - a ≠ 0 := by linarith
   field_simp; ring_nf
 
-lemma deriv_Km_strictAnti (ha: a ∈ (Set.Ioo 0 1)): StrictAntiOn (deriv (Km a)) (Set.Ici 1) := by
+private lemma deriv_Km_strictAnti (ha : a ∈ Set.Ioo 0 1) : StrictAntiOn (deriv (Km a)) (Set.Ici 1) := by
   apply strictAntiOn_of_deriv_neg (convex_Ici 1)
   apply ContinuousOn.congr _ (deriv_Km ha)
   have : ∀ x ∈ Set.Ici 1, a + x ≠ 0 := by
@@ -56,15 +55,14 @@ lemma deriv_Km_strictAnti (ha: a ∈ (Set.Ioo 0 1)): StrictAntiOn (deriv (Km a))
   intro b hb; simp only [Set.nonempty_Iio, interior_Ici'] at hb
   simp only [deriv2_Km ha hb]
   simp only [Set.mem_Ioo, Set.mem_Ioi] at ha hb
-  have :  a * a * (1 - b) / (b * (a + b) ^ 2) =  - (a * a * (b-1) / (b * (a + b) ^ 2)) := by ring_nf
+  have : a * a * (1 - b) / (b * (a + b) ^ 2) = -(a * a * (b-1) / (b * (a + b) ^ 2)) := by ring_nf
   simp only [this, Left.neg_neg_iff, gt_iff_lt]
   have : b - 1 > 0 := by linarith
   have : a > 0 := ha.left
   have : b - a > 0 := by linarith
   positivity
 
-lemma deriv_Km_neg (ha: a ∈ (Set.Ioo 0 1)) (hb: b ∈ (Set.Ioi 1)) : deriv (Km a) b < 0 := by
-  simp only [Set.mem_Ioi] at hb
+private lemma deriv_Km_neg (ha : a ∈ Set.Ioo 0 1) (hb : 1 < b) : deriv (Km a) b < 0 := by
   have :  deriv (Km a) 1 = 0 := by
     rw [deriv_Km ha (by simp only [Set.mem_Ici, le_refl])]
     simp only [Set.mem_Ioo] at ha
@@ -74,8 +72,7 @@ lemma deriv_Km_neg (ha: a ∈ (Set.Ioo 0 1)) (hb: b ∈ (Set.Ioi 1)) : deriv (Km
   rw [← this]
   apply (deriv_Km_strictAnti ha) (by simp only [Set.mem_Ici, le_refl]) (by simp only [Set.mem_Ici]; linarith) hb
 
-
-lemma Km_strictAnti (ha: a ∈ (Set.Ioo 0 1)): StrictAntiOn (Km a) (Set.Ici 1) := by
+private lemma Km_strictAnti (ha : a ∈ Set.Ioo 0 1) : StrictAntiOn (Km a) (Set.Ici 1) := by
   apply strictAntiOn_of_deriv_neg (convex_Ici 1)
   unfold Km
   have : ∀ x ∈ Set.Ici 1, x - a ≠ 0 := by
@@ -86,19 +83,16 @@ lemma Km_strictAnti (ha: a ∈ (Set.Ioo 0 1)): StrictAntiOn (Km a) (Set.Ici 1) :
   intro b hb
   apply deriv_Km_neg ha; simp only [Set.nonempty_Iio, interior_Ici'] at hb; exact hb
 
-
-lemma Km_neg (ha: a ∈ (Set.Ioo 0 1)) (hb: b ∈ (Set.Ioi 1)) : Km a b < 0 := by
+private lemma Km_neg (ha: a ∈ (Set.Ioo 0 1)) (hb : 1 < b) : Km a b < 0 := by
   have : Km a 1 = 0 := by simp only [Km, neg_mul, neg_add_cancel, mul_one, zero_add, sub_self,
     one_mul, log_one, mul_zero, add_zero]
-  rw [← this]; simp only [Set.mem_Ioi] at hb
+  rw [← this]
   apply (Km_strictAnti ha) (by simp only [Set.mem_Ici, le_refl]) _ hb
   simp only [Set.mem_Ici]; linarith
 
-
-lemma deriv_Gm_b_pos (ha: a ∈ (Set.Ioo 0 1)) (hb: b ∈ (Set.Ioi 1))
-    : (deriv (Gm a)) b > 0 := by
+private lemma deriv_Gm_b_pos (ha : a ∈ Set.Ioo 0 1) (hb : 1 < b) :
+    0 < (deriv (Gm a)) b := by
   unfold Gm
-  simp only [Set.mem_Ioc, Set.mem_Ioi] at ha hb
   rw [deriv_div]
   have : deriv (Fm b) a ^ 2 > 0 := by apply pow_pos (deriv_Fm_a_pos ha hb)
   apply div_pos
@@ -121,20 +115,21 @@ lemma deriv_Gm_b_pos (ha: a ∈ (Set.Ioo 0 1)) (hb: b ∈ (Set.Ioi 1))
   exact differentiable_Fm_a_b ha hb
   apply ne_of_gt (deriv_Fm_a_pos ha hb)
 
-lemma Mono_Gm_b (ha: a ∈ (Set.Ioo 0 1)) : StrictMonoOn (Gm a) (Set.Ioi 1) := by
+lemma Mono_Gm_b (ha : a ∈ Set.Ioo 0 1) : StrictMonoOn (Gm a) (Set.Ioi 1) := by
   apply strictMonoOn_of_deriv_pos (convex_Ioi 1)
   unfold Gm
   apply ContinuousOn.div
-  apply ContinuousAt.continuousOn
+  apply continuousOn_of_forall_continuousAt
   intro b hb
   apply DifferentiableAt.continuousAt (differentiable_Fm_b ha (le_of_lt hb))
-  apply ContinuousAt.continuousOn
+  apply continuousOn_of_forall_continuousAt
   intro b hb
   apply DifferentiableAt.continuousAt (differentiable_Fm_a_b ha hb)
   intro b hb; apply ne_of_gt (deriv_Fm_a_pos ha hb)
   intro b hb; simp only [interior_Ioi, Set.mem_Ioi] at hb; exact deriv_Gm_b_pos ha hb
 
-lemma deriv_Fm_div_pos (ha: a ∈ (Set.Ioo 0 1)) (hb: b > 1) (hc: c > b) : deriv (fun a ↦ Fm b a / Fm c a) a > 0 := by
+lemma deriv_Fm_div_pos (ha : a ∈ Set.Ioo 0 1) (hb : 1 < b) (hc : b < c) :
+    0 < deriv (fun a ↦ Fm b a / Fm c a) a := by
   have ie : Gm a b < Gm a c := by apply Mono_Gm_b ha hb (by simp only [Set.mem_Ioi];linarith) hc
   unfold Gm at ie
   have i1 : deriv (Fm b) a > 0 := by apply deriv_Fm_a_pos ha hb
@@ -147,7 +142,6 @@ lemma deriv_Fm_div_pos (ha: a ∈ (Set.Ioo 0 1)) (hb: b > 1) (hc: c > b) : deriv
   apply differentiable_Fm_a ha (by linarith)
   apply ne_of_gt (Fm_pos ha (by linarith))
 
-
 lemma Lemma65_strictMono (hr1 : 0 < r) (hr2 : r < Δ) :
     StrictMonoOn (fun i => Qm Δ i r) (Set.Iio 0) := by
   have i1 : ∀ x ∈ Set.Iio (0:ℝ), (2:ℝ) ^ x ∈ Set.Ioo 0 1 := by
@@ -159,9 +153,9 @@ lemma Lemma65_strictMono (hr1 : 0 < r) (hr2 : r < Δ) :
     apply one_lt_rpow (by simp only [Nat.one_lt_ofNat]) hx
   apply strictMonoOn_of_deriv_pos (convex_Iio 0)
   have : ContinuousOn (fun i ↦ Qm Δ i r) (Set.Iio 0) := by
-    have : ∀ t > 0, DifferentiableOn ℝ (Em_i t) (Set.Iio 0) := by
+    have : ∀ t > 0, DifferentiableOn ℝ (fun i => Em i t) (Set.Iio 0) := by
       intro t ht
-      apply DifferentiableOn.mono (@differentiable_Em_i t (by simp only [Set.mem_Ici]; linarith))
+      apply DifferentiableOn.mono (@differentiableOn_Em_i t (by linarith))
       exact subset_refl _
     unfold Qm; apply ContinuousOn.div
     apply DifferentiableOn.continuousOn (this r hr1)
@@ -174,7 +168,7 @@ lemma Lemma65_strictMono (hr1 : 0 < r) (hr2 : r < Δ) :
   have : Set.EqOn (fun i ↦ Qm Δ i r) ((fun a => Fm (2^r) a / Fm (2^Δ) a) ∘ (fun i=> 2^i)) (Set.Iio 0) := by
     intro i hi; simp only [Function.comp_apply]; rw [Qm_of_Fm]; simp only [Function.comp_apply]
     simp only [Set.mem_Iio]; simp only [Set.mem_Iio] at hi; linarith; linarith; linarith
-  rw [deriv_EqOn_Iio this hx, deriv.comp]
+  rw [deriv_EqOn_Iio this hx, deriv_comp]
   apply mul_pos
   any_goals have hx : x ∈ Set.Iio 0 := by simp only [Set.mem_Iio] ; linarith
   apply deriv_Fm_div_pos (i1 x hx) (i2 r hr1)
